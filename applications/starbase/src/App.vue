0b1c1003eb84b0f5fa4f769060c59ecb66f7380f
inibg <inibg2@gmail.com>
2018-10-09 23:14:56 -0300

    cambios que no funcionan

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
     <h3>{{title}}</h3>
+    <Character /> 
   </div>
 </template>
 
 <script>
+  import Character from './components/Character.vue'
+
   export default {
     name: 'app',
     data() {
       return {
         title: 'Generate Your Team'
+      }
+    },
+    components: {
+      Character
+    },
+    methods: {
+      created: function() {
+        console.log('holitas')
       }
     }
   }
