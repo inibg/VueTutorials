0448f261c23796167c410bfc50c0d3b4638ee1a5
inibg <inibg2@gmail.com>
2018-10-15 17:07:35 -0300

    instalación de componentes externos

---
+++ applications/turnout/src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyASt-yd6dgLypuN6QbUdq5xWJKyl--FnNU",
+  authDomain: "turnout-vue-js.firebaseapp.com",
+  databaseURL: "https://turnout-vue-js.firebaseio.com",
+  projectId: "turnout-vue-js",
+  storageBucket: "turnout-vue-js.appspot.com",
+  messagingSenderId: "418198280865"
+};
+
+export const firebaseApp = firebase.initializeApp(config)
