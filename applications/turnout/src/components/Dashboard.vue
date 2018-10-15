93cc076e50eb533a86fccb1c21ce15d1654e6917
inibg <inibg2@gmail.com>
2018-10-15 18:25:02 -0300

    Se agregan eventos

--- applications/turnout/src/components/Dashboard.vue
+++ applications/turnout/src/components/Dashboard.vue
@@ -7,12 +7,12 @@
     <hr>
     <AddEvent /> 
     <hr>
-    {{$store.state}}
+    {{$store.state.events}}
   </div>
 </template>
 
 <script>
-import { firebaseApp } from '../firebaseApp'
+import { firebaseApp, eventsRef } from '../firebaseApp'
 import AddEvent from './AddEvent.vue'
 
 export default {
@@ -24,6 +24,15 @@ export default {
   }, 
   components: {
     AddEvent
+  },
+  mounted() {
+    eventsRef.on('value', snap => {
+      let events = []
+      snap.forEach(event => {
+        events.push(event.val())
+      })
+      this.$store.dispatch('setEvents', events)
+    })
   }
 }
 </script>
